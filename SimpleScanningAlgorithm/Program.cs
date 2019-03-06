545fb743955be73d7b0caaa65629c99271b90328
Samuel Lane <samuel.lane.sa@outlook.com>
2019-03-06 16:15:31 -0500

    SimpleScanningAlgorithm

--- SimpleScanningAlgorithm/Program.cs
+++ SimpleScanningAlgorithm/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Collections.Generic;
+
 
 
 namespace SimpleScanningAlgorithm
@@ -15,18 +17,6 @@ namespace SimpleScanningAlgorithm
 
         static void Main(string[] args)
         {
-            
-
-            Console.WriteLine();
-            foreach (string i in taskList)
-            {
-                if (i == null)
-                    Console.WriteLine("(null)");
-                else
-                    Console.WriteLine("\"{0}\"", i);
-            }
-
-
             //int userResponse = 0;
             //Prompt user for name
             Console.WriteLine("Please Enter Your Name");
@@ -59,66 +49,46 @@ namespace SimpleScanningAlgorithm
                 Console.WriteLine("Invalid Option");
         }
 
-       
-        
+
+
 
         public static void addTask()
         {
             //var taskList = new List<string>();
 
-            int listNumber = 1;
+            int listNumber = 0;
             string userEntry;
             int count = 0;
-            int userAnswer = 0;
-            string userList;
-
-
-
-            //Ask if user will be adding a task to list
-            Console.WriteLine("You have selected to add a task.  Please enter in the task you would like to add. Press 0 to exit Application");
-            Console.WriteLine("-------------------------------------------------------------------------------------------------------------n/");
-
-            //Accept user entry and add to list
-            userEntry = Console.ReadLine();
-
-            Console.WriteLine($"You have entered : {listNumber}. {userEntry}");
-
-            int userResponse = 0;
+            int userResponse = 1;
 
             do
             {
+                count++;
+                listNumber++;
+
+                //Ask if user will be adding a task to list
+                Console.WriteLine("You have selected to add a task.  Please enter in the task you would like to add. Press 0 to exit Application");
+                Console.WriteLine("-------------------------------------------------------------------------------------------------------------\n");
+
+                //Accept user entry and add to list
+                userEntry = Console.ReadLine();
+
+                Console.WriteLine($"You have entered : {listNumber}. {userEntry}");
+                // Store task inside of the list
+                taskList.Add(userEntry);
+
+
                 //int userResponse;
                 //Promt user if they want to enter another task
                 Console.WriteLine("Thank you, Would like to add another task? Enter 1 for YES and 2 for NO");
-                Console.WriteLine("-------------------------------------------------------------------------------------------------------------n/");
+                Console.WriteLine("-------------------------------------------------------------------------------------------------------------\n");
 
 
                 userResponse = Convert.ToInt32(Console.ReadLine());
-
-                if (userResponse == 1)
-                {
-                    count++;
-                    listNumber++;
-
-                    //Ask if user will be adding a task to list
-                    Console.WriteLine("You have selected to add a task.  Please enter in the task you would like to add. Press 0 to exit Application");
-                    Console.WriteLine("-------------------------------------------------------------------------------------------------------------n/");
-
-                    //Accept user entry and add to list
-                    userEntry = Console.ReadLine();
-
-                    Console.WriteLine($"You have entered : {listNumber}. {userEntry}");
-                    // Store task inside of the list
-                    taskList.Add(userEntry);
-
-                }
-                else if (userResponse == 2)
-                {
-                    quitApp();
-                }
-
             } while (userResponse != 2);
 
+
+            quitApp();
 
         }
 
@@ -126,7 +96,7 @@ namespace SimpleScanningAlgorithm
         {
             //Ask if user has completed a task and will be marking it off the list
             Console.WriteLine("You have selected to remove a task from the list. Which task would you like to remove?");
-            Console.WriteLine("-------------------------------------------------------------------------------------------------------------n/");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------\n");
 
             string removeTask = Convert.ToString(Console.ReadLine());
         }
@@ -134,19 +104,23 @@ namespace SimpleScanningAlgorithm
         public static void quitApp()
         {
             //Ask user if he/she would like to quit the application
-            Console.WriteLine("Would you like to quit the application? Press 1 for Yes and 2 for No");
-            Console.WriteLine("-------------------------------------------------------------------------------------------------------------n/");
+            Console.WriteLine("Are you sure you would like to quit the application? Press 1 for Yes and 2 for No");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------\n");
 
             string quitApp = Convert.ToString(Console.ReadLine());
             Console.WriteLine("Thank you, until next time stay safe!!!");
+
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------------\n");
+
+            Console.WriteLine("Your task list has been updated");
+            foreach (string i in taskList)
+            {
+                if (i == null)
+                    Console.WriteLine("(null)");
+                else
+                    Console.WriteLine("\"{0}\"", i);
+            }
         }
-
-
-
-
-
-
-
     }
 
 }
